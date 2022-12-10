3381407f41e40a26c1604cb0c253463cd5983111
“Salil48” <salilchharia@gmail.com>
2022-12-10 17:12:46 +0530

    Added: Store the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -21,11 +21,11 @@ totalSalary=0;
 totalWorkingHour=0;
 day=1;
 
-while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+while [[ $day -le 20 && $totalWorkingHour -lt 100 ]]
 do
 	wHour=$(calculateWorkingHour $((RANDOM%3)));
 	totalWorkingHour=$(($totalWorkingHour + $wHour));
-	if [ $totalWorkingHour -gt 40 ]
+	if [ $totalWorkingHour -gt 100 ]
 	then
 		totalWorkingHour=$(($totalWorkingHour - $wHour));
 		break;
@@ -33,6 +33,8 @@ do
 	salary=$(($perHourSalary * $wHour));
 	totalSalary=$(($totalSalary + $salary));
 	((day++));
+echo "Daily wage:"${salary[@]}
+echo "Total wage:"${totalSalary[@]}
 done
 echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
 
